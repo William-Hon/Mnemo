b3726c3bacd471d196f9b1e2991d65f4de7097fd
William Hon <32whon@gmail.com>
2026-09-01 04:48:11 -0400

    force MNEMO tab title

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,9 +2,9 @@ import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="sign-in" />
-      <Stack.Screen name="sign-up" />
+    <Stack screenOptions={{ headerShown: false, title: 'MNEMO' }}>
+      <Stack.Screen name="sign-in" options={{ title: 'MNEMO' }} />
+      <Stack.Screen name="sign-up" options={{ title: 'MNEMO' }} />
     </Stack>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { useFonts } from 'expo-font';
-import { Stack, useRouter, useSegments, ThemeProvider, DarkTheme } from 'expo-router';
+import { Stack, useRouter, useSegments, ThemeProvider, DarkTheme, Head } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import '../global.css';
@@ -55,6 +55,9 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
+      <Head>
+        <title>MNEMO</title>
+      </Head>
       <RootLayoutNav />
     </AuthProvider>
   );
